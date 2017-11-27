783dd6b15d76797d616f0d1ede2932e7fcbacd3c
Dmitry Ermakov <zetoke@gmail.com>
2017-11-27 17:44:33 +0300

    Add typings

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+export interface LaunchDarklyUser {
+    key: string;
+    email?: string;
+    firstName?: string;
+    lastName?: string;
+    isAnonymous?: boolean;
+  }
+
+export function configure(apiKey: string, user: LaunchDarklyUser): void;
+export function boolVariation(flagName: string, callback: (status: boolean) => any): boolean;
+export function stringVariation(flagName: string, fallback: string, callback: (status: string) => void): string;
+export function addFeatureFlagChangeListener(flagName: string, callback: (flagName: string) => void): void;
+export function unsubscribe(): void;
